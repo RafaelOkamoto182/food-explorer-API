d869c386ce9f12ce593f280dd8ae3ca696fe56b3
Rafael Okamoto <rafa.mg1@hotmail.com>
2023-12-18 22:41:41 -0300

    adicionado alguns onDelete nas tabelas auxiliares

--- src/database/knex/migrations/20231218210109_createDishes_ingredients.js
+++ src/database/knex/migrations/20231218210109_createDishes_ingredients.js
@@ -2,7 +2,7 @@
 exports.up = knex.schema.createTable("dishes_ingredients", table => {
 
     table.integer("dish_id").references("id").inTable("dishes");
-    table.integer("ingredient_id").references("id").inTable("ingredients");
+    table.integer("ingredient_id").references("id").inTable("ingredients").onDelete("CASCADE");
     table.primary(["dish_id", "ingredient_id"]);
 
 });
--- src/database/knex/migrations/20231218210122_createDishes_categories.js
+++ src/database/knex/migrations/20231218210122_createDishes_categories.js
@@ -2,7 +2,7 @@
 exports.up = knex.schema.createTable("dishes_categories", table => {
 
     table.integer("dish_id").references("id").inTable("dishes");
-    table.integer("category_id").references("id").inTable("categories");
+    table.integer("category_id").references("id").inTable("categories").onDelete("CASCADE");
     table.primary(["dish_id", "category_id"]);
 
 });
