135f1f433ec5ea7b0a65b98097fa59c2d6f90099
Rafael Okamoto <rafa.mg1@hotmail.com>
2023-12-23 20:07:53 -0300

    removed the category routes and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const sessionsRouter = require("./sessions.routes");
 const dishesRouter = require("./dishes.routes");
-const categoriesRoutes = require("./categories.routes");
 const ingredientsRoutes = require("./ingredients.routes");
 
 
@@ -12,7 +11,6 @@ const routes = Router();
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/dishes", dishesRouter);
-routes.use("/categories", categoriesRoutes)
-routes.use("/ingredients", ingredientsRoutes)
+routes.use("/ingredients", ingredientsRoutes);
 
 module.exports = routes;
