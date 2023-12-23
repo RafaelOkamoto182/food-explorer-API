0ab699a45748fddb74ffe9c0740ec5beb5b7c6e8
Rafael Okamoto <rafa.mg1@hotmail.com>
2023-12-23 20:27:07 -0300

    feat: added authentication middleware to the needed routes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
+
 
 const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 const upload = multer(uploadConfig.MULTER)
 
 const DishesController = require("../controllers/DishesController")
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const dishesController = new DishesController()
 
 const dishesRoutes = Router()
@@ -13,9 +15,9 @@ const dishesRoutes = Router()
 
 dishesRoutes.use(ensureAuthenticated)
 
-dishesRoutes.post("/", upload.single("dish_picture"), dishesController.create);
+dishesRoutes.post("/", verifyUserAuthorization(["admin"]), upload.single("dish_picture"), dishesController.create);
 dishesRoutes.get("/:id", dishesController.getById)
 dishesRoutes.get("/", dishesController.get)
-dishesRoutes.delete("/:id", dishesController.delete)
+dishesRoutes.delete("/:id", verifyUserAuthorization(["admin"]), dishesController.delete)
 
 module.exports = dishesRoutes
--- src/routes/ingredients.routes.js
+++ src/routes/ingredients.routes.js
@@ -1,13 +1,15 @@
 const { Router } = require("express")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
 const IngredientsController = require("../controllers/IngredientsController")
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const ingredientsController = new IngredientsController
 
 const ingredientsRoutes = Router()
 
 
 ingredientsRoutes.use(ensureAuthenticated)
+ingredientsRoutes.use(verifyUserAuthorization(["admin"]))
 
 ingredientsRoutes.post("/", ingredientsController.create)
 ingredientsRoutes.delete("/:id", ingredientsController.delete)
